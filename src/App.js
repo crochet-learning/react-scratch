ffff93c7123921df185d4b0dfa7b53e2a971c11d
crochetcode <fred.davison@outlook.com>
2022-06-18 09:26:02 -0500

    :sparkles: init repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import {hot} from 'react-hot-loader';
+
+const App = () => {
+    return (
+        <div>App</div>
+    )
+}
+
+export default hot(module)(App);
---
+++ src/README.md
@@ -0,0 +1,3 @@
+from the Medium article "Creating a React App… From Scratch" by Jedai Saboteur
+
+[Link](https://medium.com/@JedaiSaboteur/creating-a-react-app-from-scratch-f3c693b84658)
